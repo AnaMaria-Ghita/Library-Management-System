080861726c429bb84537708dfe8917076402da48
Ana Ghita <anaghita78@gmail.com>
2024-12-02 12:05:34 +0200

    added methods for Book class

--- project.cpp
+++ project.cpp
@@ -11,6 +11,7 @@ private:
     bool isAvailable;
 public:
     static int numberBooks;
+    static int numberUsers;
     //constructors
     Book(const int id, const char* title, const char* author, const char* genre, bool isAvailable): id(id), isAvailable(true){
         this->title=new char[strlen(title)+1];
@@ -35,7 +36,7 @@ public:
         delete[] genre;
     }
 
-    //constructor de citire
+    //constructor de afisare
     friend ostream& operator<<(ostream& os, Book& book){
         os<<"ID: "<< book.id <<endl;
         os<<"Title: "<<book.title<<endl;
@@ -51,12 +52,35 @@ public:
 
         return os;
     }
+
+    friend istream& operator>>(istream& is, Book& book){
+        cout << "Write the new genre of the book: ";
+        is>>book.genre;
+
+        return is;
+    }
+
+    //methods
+     bool checkAvailability(){
+     return isAvailable;
+    }
+    void markBorrowed(){
+        isAvailable=false;
+    }
+    void markReturned(){
+        isAvailable=true; 
+    }
+
 };
 
 int Book::numberBooks = 0;
+int Book::numberUsers = 0;
+
 int main(){
     Book b1(100, "Alice's Adventures in Wonderland", "Lewis Carrol", "Fantasy", true );
     cout << b1 << endl;
+    cin>>b1;
+    cout << b1;
 
     return 0;
 }
